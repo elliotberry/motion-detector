8c08d51b9c9b7229d05fa4bdc82c58b7d6740eda
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2025-04-27 18:01:56 -0400

    added mpg123, upped range of mp3s

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import Fastify from 'fastify';
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import {exec} from 'child_process';
+import {promisify} from 'util';
 import os from 'node:os';
 import crypto from 'node:crypto';
+import pino from 'pino';
+
+const execAsync = promisify(exec);
+const platform = os.platform();
+
+const fileTransport = pino.transport({
+  target: 'pino/file',
+  level: 'info',
+  options: {destination: `./app.log`},
+});
 
 const fastify = Fastify({
   logger: {
@@ -10,17 +20,17 @@ const fastify = Fastify({
       target: 'pino-pretty',
       options: {
         colorize: true,
-        translateTime: 'yyyy-mm-dd hh:MM:ss TT Z',
+        translateTime: 'SYS:yyyy-mm-dd hh:MM:ss',
         ignore: 'pid,hostname',
       },
     },
-    timestamp: () => `,"time":"${new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })}"`,
+
+    fileTransport,
+    timestamp: () => `,"time":"${new Date().toLocaleString('en-US', {timeZone: 'America/New_York'})}"`,
   },
 });
-const execAsync = promisify(exec);
-const platform = os.platform();
 
-const pickSound = (ip) => {
+const pickSound = ip => {
   const hash = crypto.createHash('md5').update(ip).digest();
   const firstByte = hash[0];
   return firstByte % 2 === 0 ? '1.mp3' : '2.mp3';
@@ -30,23 +40,21 @@ fastify.get('/', async (request, reply) => {
   try {
     request.log.info('Received request from', request.ip);
 
-    const command = platform === 'darwin' ? 'afplay -v 100' : 'aplay';
+    const command = platform === 'darwin' ? 'afplay -v 100' : 'mpg123';
     const soundFile = pickSound(request.ip);
 
     await execAsync(`${command} ./` + soundFile);
     request.log.info('Played sound.');
-    reply.send({ status: 'ok' });
+    reply.send({status: 'ok'});
   } catch (err) {
     request.log.error(err);
-    reply.status(500).send({ error: 'Failure' });
+    reply.status(500).send({error: 'Failure'});
   }
 });
 
 const main = async () => {
   try {
-    console.log('Starting server on', platform);
-    await fastify.listen({ port: 3030, host: '0.0.0.0' });
-    console.log(`Listening on ${fastify.server.address().address}:${fastify.server.address().port}`);
+    await fastify.listen({port: 3030, host: '0.0.0.0'}); 
   } catch (err) {
     console.error(err);
     process.exit(1);
